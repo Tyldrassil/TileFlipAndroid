26e37aad5de76c7a6477a16bf46ff31d1cf5d5a3
Tyldrassil <hyperkirbytwo@gmail.com>
2022-12-19 10:04:26 +0100

    flipAdjacent tested and fixed

--- app/src/main/java/no/thDitman/tileFlipFun/classes/Board.kt
+++ app/src/main/java/no/thDitman/tileFlipFun/classes/Board.kt
@@ -3,9 +3,10 @@ package no.thDitman.tileFlipFun.classes
 class Board constructor(size: Int) {
 
     val board: MutableList<Boolean>
-    val size: Int = 0
+    var boardSize: Int = 0
 
     init {
+        boardSize = size
         board = object : ArrayList<Boolean>() {
             //This is for initializing at a variable length
             init {
@@ -28,6 +29,24 @@ class Board constructor(size: Int) {
     }
 
     fun flipAdjacent(index: Int) {
-        //TODO
+        //Makes sure left of tapped is valid
+        if (index % boardSize != 0 && index != 0) {
+            flipSingle(index-1)
+        }
+
+        //Makes sure above of tapped is valid
+        if (index >= boardSize) {
+            flipSingle(index-boardSize)
+        }
+
+        //Makes sure right of tapped is valid
+        if (index % boardSize != 2) {
+            flipSingle(index+1)
+        }
+
+        //Makes sure below of tapped is valid
+        if (index < (boardSize*boardSize)-boardSize) {
+            flipSingle(index+boardSize)
+        }
     }
 }
--- app/src/test/java/no/thDitman/tileFlipFun/logic/BoardTest.kt
+++ app/src/test/java/no/thDitman/tileFlipFun/logic/BoardTest.kt
@@ -4,7 +4,7 @@ import no.thDitman.tileFlipFun.classes.Board
 import org.junit.Assert.*
 import org.junit.Test
 
-internal class BoardTest {
+class BoardTest {
 
     @Test
     fun flipOnTap() {
@@ -32,17 +32,69 @@ internal class BoardTest {
     }
 
     @Test
-    fun flipAdjacent() {
+    fun flipAdjacentLeftInvalid() {
 
         val actual = Board(3)
 
         val expected = Board(3)
 
-        expected.board[1] = true
-        expected.board[3] = true
+        expected.board[0] = true
+        expected.board[4] = true
+        expected.board[6] = true
 
-        actual.board.flipAdjacent(0)
-        
+        actual.flipAdjacent(3)
+
+        assertEquals(expected.board.toList(), actual.board.toList())
+
+    }
+
+    @Test
+    fun flipAdjacentTopInvalid() {
+
+        val actual = Board(3)
+
+        val expected = Board(3)
+
+        expected.board[0] = true
+        expected.board[2] = true
+        expected.board[4] = true
+
+        actual.flipAdjacent(1)
+
+        assertEquals(expected.board.toList(), actual.board.toList())
+
+    }
+
+    @Test
+    fun flipAdjacentRightInvalid() {
+
+        val actual = Board(3)
+
+        val expected = Board(3)
+
+        expected.board[2] = true
+        expected.board[4] = true
+        expected.board[8] = true
+
+        actual.flipAdjacent(5)
+
+        assertEquals(expected.board.toList(), actual.board.toList())
+
+    }
+
+    @Test
+    fun flipAdjacentBotInvalid() {
+
+        val actual = Board(3)
+
+        val expected = Board(3)
+
+        expected.board[4] = true
+        expected.board[6] = true
+        expected.board[8] = true
+
+        actual.flipAdjacent(7)
+
         assertEquals(expected.board.toList(), actual.board.toList())
 
     }
